edecd03e5571647e9b21145b7afe5e103e3962b5
Brasilino de Souza Neto <neto.bq@gmail.com>
2024-08-07 16:46:54 -0300

    teste

---
+++ CasaDoCodigo/Contexto.cs
@@ -0,0 +1,29 @@
+﻿using CasaDoCodigo;
+using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Data.Entity;
+using System;
+
+namespace CasaDoCodigo
+{
+    public class Contexto : System.Data.Entity.DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Produto>? Produtos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<ItemPedido>? ItensPedido { get; set; }
+
+
+        public Contexto(DbContextOptions<Contexto> options)
+            : base()
+        {
+
+        }
+
+    }
+
+}
+
+
+    
+
+
--- CasaDoCodigo/Program.cs
+++ CasaDoCodigo/Program.cs
@@ -1,3 +1,8 @@
+using CasaDoCodigo;
+using Microsoft.EntityFrameworkCore;
+using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +17,19 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddMvc();
+    String connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CasaDoCodigo;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    services.AddDbContext<Contexto>(options => options.UseSqlServer(connectionString));
+}
+
+
+builder.Services.AddDbContext<Contexto>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString(@"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CasaDoCodigo;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"));
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
