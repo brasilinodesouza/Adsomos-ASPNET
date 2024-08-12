4d0cbf5feef127c1cd85e51289db779f8e155e5f
Brasilino de Souza Neto <neto.bq@gmail.com>
2024-08-12 11:17:43 -0300

    adsomos

--- CasaDoCodigo/Models/ItemPedido.cs
+++ CasaDoCodigo/Models/ItemPedido.cs
@@ -4,8 +4,7 @@ namespace CasaDoCodigo.Models
 {
     public class ItemPedido : BaseModel
     {
-        [DataMember]
-        public Pedido Pedido { get; private set; }
+       
         [DataMember]
         public Produto Produto { get; private set; }
         [DataMember]
@@ -22,19 +21,19 @@ namespace CasaDoCodigo.Models
             this.Quantidade = quantidade;
         }
 
-        public ItemPedido(int id, Pedido pedido, Produto produto, int quantidade) : this(pedido, produto,quantidade)
+        public ItemPedido(int id, Produto produto, int quantidade) : this(produto,quantidade)
         {
             this.Id = id;
 
         }
 
         public ItemPedido() { }
-        public ItemPedido(Pedido pedido, Produto produto, int quantidade)
+        public ItemPedido( Produto produto, int quantidade)
         {
             this.Produto = produto;
             this.Quantidade = quantidade;
             this.PrecoUnitario = produto.Preco;
-            this.Pedido = pedido;
+            
             
         }
 
